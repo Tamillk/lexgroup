3fc40ce2eed0498171d838f01b1b8e02eca6f38f
Tamillk <122620722+Tamillk@users.noreply.github.com>
2025-01-24 21:53:19 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const splash = document.getElementById('splash-screen');
-    const mainContent = document.getElementById('main-content');
-    setTimeout(() => {
-        splash.style.display = 'none';
-        mainContent.style.display = 'block';
-    }, 3000); // Splash screen duration: 3 seconds
-});
