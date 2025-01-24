52aa5eb990750d8a8c0fb1bb4fcea930f92ba6e2
Tamillk <122620722+Tamillk@users.noreply.github.com>
2025-01-24 21:29:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const target = document.querySelector(event.target.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
